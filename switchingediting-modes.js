3499d47970d7e664cdb19464e62b076bee9ad257
anidiifeanyi <anidiifeanyi@gmail.com>
2024-10-09 13:40:01 +0100

    added function to open and close browser

--- switchingediting-modes.js
+++ switchingediting-modes.js
@@ -1,5 +1,18 @@
 import puppeteer from 'puppeteer'
 
-const switchEditorMode = async () => {}
+const switchEditorMode = async () => {
+	let browser
+	let page
+
+	initiateTest(async () => {
+		browser = await puppeteer.launch({ headless: false }) // Set to false to observe the test visually
+		page = await browser.newPage()
+		await page.goto('http://your-document-editor-url') // Replace with the actual URL
+	})
+
+	endTest(async () => {
+		await browser.close()
+	})
+}
 
 switchEditorMode()
